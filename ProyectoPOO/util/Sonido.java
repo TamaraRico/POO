32abfb24f275eb7571e3b9c282e91a177a8d895a
TamaraRico <71352975+TamaraRico@users.noreply.github.com>
2021-06-01 06:52:19 -0700

    Add files via upload

---
+++ ProyectoPOO/util/Sonido.java
@@ -0,0 +1,49 @@
+package util;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+
+public class Sonido {
+	private Clip clip;
+	//private FloatControl volumen;
+	
+	public Sonido(Clip clip) {
+		this.clip=clip;
+		//volumen=(FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+	}
+	
+	public void play() {
+		/*if(clip==null) return;
+		stop();*/
+		clip.setFramePosition(0);
+		clip.start();
+	}
+	
+	public void playwF(int frame) {
+		clip.setFramePosition(frame);
+		clip.start();
+	}
+	
+	/*public void close() {
+		stop();
+		clip.close();
+	}*/
+	
+	public void loop() {
+		clip.setFramePosition(0);
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+	
+	public void stop() {
+		clip.stop();
+		//if(clip.isRunning()) clip.stop();
+	}
+	
+	/*public void changeVolume(float value) {
+		volumen.setValue(value);
+	}*/
+
+}
